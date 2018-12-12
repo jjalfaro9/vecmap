48a7fce2f71a85b9cecf8f26db7db9c4aa7264a0
Atomkovich <Alexander.Tomkovich@utdallas.edu>
2018-12-12 12:57:30 -0600

    Stuff

--- map_embeddings.py
+++ map_embeddings.py
@@ -373,7 +373,7 @@ def main():
                 u, s, vt = xp.linalg.svd(m, full_matrices=False)
                 print("S", s)
                 print("VT", vt)
-                return vt.T.dot(xp.diag(1/(s + 1e-5)).dot(vt))
+                return vt.T.dot(xp.diag(1/(s + 1e-3)).dot(vt))
 
             if args.whiten:
                 wx1 = whitening_transformation(xw[src_indices])
