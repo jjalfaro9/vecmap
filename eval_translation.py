11fdd2812972ca5be56f276576706dba86df8d16
Atomkovich <Alexander.Tomkovich@utdallas.edu>
2018-12-12 22:09:54 -0600

    Stuff

--- eval_translation.py
+++ eval_translation.py
@@ -177,7 +177,7 @@ def eval_translation(src_emb, trg_emb, use_file_emb, test_dict, rpt_file, run_ms
 
         for i in correct:
             print(src_words[i])
-            trans_out.write(src_words[i].encode("utf-8") + ": System{" + trg_words[translation[i]].encode("utf-8") + "} | Gold{" + str([trg_words[item].encode("utf-8") for item in src2trg[i]]) + "}\n")
+            trans_out.write(u''.join(src_words[i].encode("utf-8"), u": System{", trg_words[translation[i]].encode("utf-8"), u"} | Gold{", str([trg_words[item].encode("utf-8") for item in src2trg[i]]), u"}\n"))
         trans_out.write("##########Incorrect:\n")
         for i in incorrect:
             trans_out.write(src_words[i].encode("utf-8") + ": System{" + trg_words[translation[i]].encode("utf-8") + "} | Gold{" + str([trg_words[item].encode("utf-8") for item in src2trg[i]]) + "}\n")
